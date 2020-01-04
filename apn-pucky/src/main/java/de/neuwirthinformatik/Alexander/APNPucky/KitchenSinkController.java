b2d710853b140ff1d5ceee4164adadfb44e26b00
APN-Pucky <APN-Pucky@users.noreply.github.com>
2020-01-04 21:11:12 +0100

    Add ComDev

--- apn-pucky/src/main/java/de/neuwirthinformatik/Alexander/APNPucky/KitchenSinkController.java
+++ apn-pucky/src/main/java/de/neuwirthinformatik/Alexander/APNPucky/KitchenSinkController.java
@@ -1302,7 +1302,6 @@ public class KitchenSinkController {
 			return url;
 		return null;
 	}
-
 	private static String getRoadMapUrl() {
 		String general = Wget.wGet("https://www.kongregate.com/forums/2468-general");
 		String[] lines = general.split("\n");
@@ -1313,8 +1312,20 @@ public class KitchenSinkController {
 				break;
 			}
 		}
-		String url = "https://www.kongregate.com"
-				+ fin.substring(fin.indexOf("href=\"/forums/2468-general") + 6, fin.indexOf("\">[Dev]"));
+		String[] guesses = new String[] {"Dev","ComDev", "COMDEV","DEV"};
+		String url = "";
+		try {
+			for(String g : guesses) {
+				if(!url.equals(""))break;
+				url = "https://www.kongregate.com"
+						+ fin.substring(fin.indexOf("href=\"/forums/2468-general") + 6, fin.indexOf("\">["+g+"]"));
+			}
+		}
+		catch(Exception e) {
+
+			url = "https://www.kongregate.com"
+					+ fin.substring(fin.indexOf("href=\"/forums/2468-general") + 6, fin.indexOf("\">[COMDEV]"));
+		}
 		return url;
 	}
 
